f57ef8f4e10b6c019249e9aabb3299f13be22134
Lidia Butkovic <lbutkovi1@student.unipu.hr>
2026-07-20 22:51:26 +0200

    Inicijalni Vue projekt s Tailwind CSS i Firebase

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import './assets/main.css'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: []
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
